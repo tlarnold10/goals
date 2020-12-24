5cfdfab2526d652a5f101cb39cf6054a7cc368f6
tlarnold10 <tlarnold10@hotmail.com>
2020-12-24 13:13:18 -0600

    goal model built

--- goals/mygoals/models.py
+++ goals/mygoals/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Goal(models.Model):
+    summary = models.CharField(max=100)
+    details = models.CharField(max=1000)
+
+    def __str__(self):
+        return self.summary
+
+# class ActionStep(models.Model):
+#     details = models.CharField(max=500)
+#     goal = models.ForeignKey(
+#         "info.Author", related_name="books", on_delete=models.CASCADE)
