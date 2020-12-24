e19262acdf0598dada1b8fb8cf01a0b891f0c5f9
TrevorArnold <Trevor.Arnold@coyotes.usd.edu>
2020-12-24 16:24:17 -0600

    added schema for goal steps

--- goals/mygoals/schema.py
+++ goals/mygoals/schema.py
@@ -15,8 +15,12 @@ class StepType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_goals = graphene.List(GoalType)
+    all_steps = graphene.List(StepType)
 
     def resolve_all_goals(root, info):
         return Goal.objects.all()
+
+    def resolve_all_steps(root, info):
+        return Step.objects.all()
 
 schema = graphene.Schema(query=Query)
