3e5718bf1b6eb34b5ebeba77d48573a42c4c8e3e
tlarnold10 <tlarnold10@gmail.com>
2021-01-04 20:45:00 -0600

    trying things

--- goals/goals/wsgi.py
+++ goals/goals/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goals.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goals.settings')
 
-# application = get_wsgi_application()
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
