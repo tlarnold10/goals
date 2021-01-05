a8bc26aa6f53f27d1ac69c65397cf7e0be85d352
tlarnold10 <tlarnold10@gmail.com>
2021-01-05 11:16:51 -0600

    working on things

--- goals/manage.py
+++ goals/manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goals.settings')
+    try:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goals.settings')
+    except:
+        print("FFFFFFFFFFFFFFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUUUUUCCCCCCCCCCCCCCCCCCCCKKKKKKKKKKKKKK")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
