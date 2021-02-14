fecbb8dec29760438a3c6700e56df2c970f90057
tlarnold10 <tlarnold10@gmail.com>
2021-02-14 16:20:25 -0600

    added react build

--- goals/settings.py
+++ goals/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REACT_APP_PATH = os.path.join(BASE_DIR, 'frontend')
--- mygoals/views.py
+++ mygoals/views.py
@@ -4,14 +4,17 @@ import logging
 from django.http import HttpResponse
 from django.views.generic import View
 from django.conf import settings
+import pdb
+
 class FrontendAppView(View):
     """
     Serves the compiled frontend entry point (only works if you have run `yarn
     build`).
     """
-    index_file_path = os.path.join(settings.REACT_APP_PATH, 'app/public', 'index.html')
+    index_file_path = os.path.join(settings.STATICFILES_DIRS[0], 'index.html')
     def get(self, request):
         try:
+            print(self.index_file_path)
             with open(self.index_file_path) as f:
                 return HttpResponse(f.read())
         except FileNotFoundError:
